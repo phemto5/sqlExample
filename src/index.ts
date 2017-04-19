10272ee4a808a2358fb5e171aea466da2904b952
Nick Anderson <NAnderson@wagstaff.com>
2017-04-19 13:50:15 -0700

    update to have let console spew

--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,16 @@ import { Day } from './Day';
 import * as db from './Database';
 import { LogFile } from './LogFile';
 
-console.log(`Started Server ${new Date()}`);
+// console.log(`Started Server ${new Date()}`);
 let config = require('./config.json');
-console.log(`Config Loaded`);
-console.log(config);
+// console.log(`Config Loaded`);
+// console.log(config);
 let processing: boolean = false
-console.log(`Processing : ${processing}`);
-console.log(`Setting up Intervals`);
+// console.log(`Processing : ${processing}`);
+// console.log(`Setting up Intervals`);
 setInterval(() => {
     if (!processing) {
-        console.log(`${processing} ...\n\tStarting next process`);
+        // console.log(`${processing} ...\n\tStarting next process`);
         var days = new LogFile(config.filePath).days;
         days.forEach(day => {
             // db.UpsertDay(day);//not readty to send to databse yet
@@ -20,6 +20,6 @@ setInterval(() => {
             // console.log(`${day.getDate()} solidworks ${day.getHighMark('solidworks')}`);
         })
     } else {
-        console.log(`${processing} ...\n\tDelaying next process for ${config.delaySeconds} seconds`);
+        // console.log(`${processing} ...\n\tDelaying next process for ${config.delaySeconds} seconds`);
     }
 }, 1000 * config.delaySeconds);
